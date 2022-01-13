9fff9f1d98fa676f1fbac3b0ea085aecc138f6e5
Aleksandra Obriadina <xbreathoflife@gmail.com>
2022-01-14 02:03:58 +0300

    remove import

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -7,7 +7,6 @@ import (
 	"github.com/xbreathoflife/url-shortener/internal/app/handler"
 	"github.com/xbreathoflife/url-shortener/internal/app/storage"
 	"net/http"
-	"strconv"
 )
 
 type urlServer struct {
