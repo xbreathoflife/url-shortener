f0542f4bb9d9008c0f15969a0987d0c17d36bec7
Aleksandra Obriadina <xbreathoflife@gmail.com>
2022-01-16 20:06:40 +0300

    add flags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"flag"
 	"github.com/xbreathoflife/url-shortener/config"
 	"github.com/xbreathoflife/url-shortener/internal/app/server"
 	"log"
 	"net/http"
 )
 
+func parseFlags(conf *config.Config) {
+	address := flag.String("a", "", "Адрес запуска HTTP-сервера")
+	baseURL := flag.String("b", "", "Базовый адрес результирующего сокращённого URL")
+	filePath := flag.String("f", "", "Путь до файла с сокращёнными URL")
+	flag.Parse()
+
+	if *address != "" {
+		conf.Address = *address
+	}
+
+	if *baseURL != "" {
+		conf.BaseURL = *baseURL
+	}
+
+	if *filePath != "" {
+		conf.FilePath = *filePath
+	}
+}
+
 func main() {
 	conf := config.Init()
+	parseFlags(&conf)
 	urlServer := server.NewURLServer(conf.BaseURL, conf.FilePath)
 	r := urlServer.URLHandler()
 	log.Fatal(http.ListenAndServe(conf.Address, r))
